984de051efac478e24f383cbd7441c7a7aa4a10c
Vinicius Araujo <vinipitta@gmail.com>
2021-05-29 22:46:05 -0300

    Solution removed from README

--- reverse-array-with-no-additional-variables/main.js
+++ reverse-array-with-no-additional-variables/main.js
@@ -2,14 +2,18 @@ function revert(input) {
     let buffer = '';
     let i, j;
   
+    // reverse whole array: words will be backwards 
     for (i = 0, j = (input.length - 1); i < (input.length / 2); i++, j--) {
         buffer = input[i];
         input[i] = input[j];
         input[j] = buffer;
     }
    
+    // loop through array
     for (i = 0, j = 0; j < input.length; j++) {
+        // when found a word (hit white space or end of array)
         if (input[j] == ' ' || j == (input.length - 1)) {
+            // use i and j to reverse the found word
             if (input[j] == ' ') {
                 j = j - 1;
             }
@@ -18,7 +22,9 @@ function revert(input) {
                 input[i] = input[j];
                 input[j] = buffer;
             }
+            // get j back to original position
             for (; input[j] != ' ' && j < input.length; j++);
+            // put i ahead o j for next word reversing
             i = j + 1;   
         } 
     }
@@ -27,6 +33,6 @@ function revert(input) {
 }
 
 const printable = (arr) => arr.reduce((acc, v) => `${acc }${v}`, '');
-let input1 = ['l', 'a', 'z','y', ' ', 'f', 'o', 'x', ' ', 'j', 'u', 'm', 'p', 'e', 'd', ' ', 'o', 'v', 'e', 'r'];
 
+let input1 = ['l', 'a', 'z','y', ' ', 'f', 'o', 'x', ' ', 'j', 'u', 'm', 'p', 'e', 'd', ' ', 'o', 'v', 'e', 'r'];
 console.log(`input: ${printable(input1)}\nreverted: ${printable(revert(input1))}`);
